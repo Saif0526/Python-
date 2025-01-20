fb05e6720006810e6dc2d8777ee59206d8c7f678
Saif ali Shaikh <94882116+Saif0526@users.noreply.github.com>
2025-01-20 11:48:19 +0530

    Add files via upload

---
+++ Variable tutorial.py
@@ -0,0 +1,39 @@
+x = 22
+print(x)
+
+y = 'chocolate'
+print(y)
+
+x,y,z = 'chocolate ','vanilla','apple'
+
+print(x)
+print(y)
+print(z)
+
+#Naming Variable
+
+#Camel case
+#Test Variable Case
+testvariableCase = 'vanilla Icecream'
+
+#Pascal  case
+#Test Variable Case
+TestvariableCase = 'Vanilla Icecream'
+
+#Snakel case
+#Test Variable Case
+test_variable_case = 'Vanilla Icecream'
+
+#operation with variable
+
+x = 3+4
+print(x)
+
+x = ' Ice Cream'
+y = ' is'
+z = ' my favorite.'
+print(x,y,z)
+
+x = 'Ice Cream'
+y = 2 
+print(x,y)
